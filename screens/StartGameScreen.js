459496045347d2b53205937dba6ab16e6b7fccc9
Yashwant <yashlande@gmail.com>
2023-11-26 13:29:49 +0530

    YashL: basic setup and screen creation

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { TextInput, View } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+
+function StartGameScreen() {
+  return (
+    <View>
+      <TextInput />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  );
+}
+
+export default StartGameScreen;
